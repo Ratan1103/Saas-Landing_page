27b2bc01efac8716527f2971129a7ca0a98b1e7c
Ratan Sanjay <sanjayratan665@gmail.com>
2025-07-01 22:33:18 +0530

    Added Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from './sections/header'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline'>Hello World</h1>
-    </div>
+    <main className="overflow-hidden">
+      <Header />
+    </main>
   )
 }
 
